a5c089b77e7c5a6d80f2434564b6f9e9b75aa519
Luca Pescatore <luca.pescatore@cern.ch>
2017-02-27 11:37:45 +0100

    Fixed minor bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 export TOOLSSYS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Variable TOOLSYS set: "$TOOLSSYS
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/user/p/pluca/repositories/tools-easyanalysis/roofit/dic
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TOOLSSYS/roofit/dic
 
 
