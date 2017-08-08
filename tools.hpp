ab225f725219c5c8b8f152242780842f6fb53189
Simone Bifani <simone.bifani@cern.ch>
2017-08-08 10:06:53 +0100

    move analysis and analyser to tools

--- tools.hpp
+++ tools.hpp
@@ -1,10 +1,13 @@
-#ifndef ANALYSIS_HPP
-#define ANALYSIS_HPP
+#ifndef TOOLS_HPP
+#define TOOLS_HPP
 
 #include "TreeReader.hpp"
 #include "ModelBuilder.hpp"
 #include "general_functions.hpp"
 #include "optimiser.hpp"
 #include "CutFlow.hpp"
+
+#include "analyser.hpp"
+#include "multi_analyser.hpp"
 
 #endif
