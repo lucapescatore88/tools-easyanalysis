9296771928f6554ca89802b05b98048e6500004b
Simone Bifani <simone.bifani@cern.ch>
2017-11-06 11:18:00 +0000

    tested Gauss, Ipatia, Ipatia2 [skip ci]

--- python/test.py
+++ python/test.py
@@ -3,5 +3,7 @@
 import os
 import easyanalysis as ea
 
+#a = ea.parser.create_analysis(os.path.expandvars("$TOOLSSYS/Tutorials/test.yaml"))
 a = ea.parser.create_analysis(os.path.expandvars("test.yaml"))
+
 a.Fit()
