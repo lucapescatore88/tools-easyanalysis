a5c5e4d971927b26c9b607c7c677e4981a1249eb
Luca Pescatore <luca.pescatore@cern.ch>
2017-11-29 09:21:37 +0000

    Update test.py

--- python/test.py
+++ python/test.py
@@ -4,6 +4,7 @@ import os
 import easyanalysis as ea
 
 #a = ea.parser.create_analysis(os.path.expandvars("$TOOLSSYS/Tutorials/test.yaml"))
-a = ea.parser.create_analysis(os.path.expandvars("test.yaml"))
+#a = ea.parser.create_analysis(os.path.expandvars("test.yaml"))
+a = ea.parser.create_analysis(os.path.expandvars("$TOOLSSYS/python/test.yaml"))
 
 a.Fit()
