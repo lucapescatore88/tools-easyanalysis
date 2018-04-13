a4c3a9396bb942eeea04e88f1df77db844a5164b
gandreas <guido.andreassi@cern.ch>
2018-04-13 15:40:00 +0200

    updated STCut

--- tools/STCut.cpp
+++ tools/STCut.cpp
@@ -2,7 +2,8 @@
 //20.02.2018
 
 #include "STCut.hpp"
-
+#include <iostream>
+#include <algorithm>
 
 //constructor with TCut
 STCut::STCut (const TCut& cut) {
@@ -16,8 +17,20 @@ STCut::STCut (const char* title) {
 
 //copy constructor
 STCut::STCut (const STCut& stcut) {
+	std::cout<<"Copy constructor called for "<<stcut.GetTitle()<<std::endl;
 	c = stcut.c;
 	members = stcut.members;
+	ptrs_to_members=stcut.ptrs_to_members;
+	pointed_by=stcut.pointed_by;
+
+	//add itself in pointed_by of the objects it points to
+	for (auto &member : ptrs_to_members){
+		if (member != NULL){
+			auto &v = member->pointed_by;
+			v.emplace_back(this);
+			std::cout<<"Spingo "<<this<<" in "<<member<<std::endl;
+		}
+	}
 }
 
 
@@ -28,16 +41,27 @@ const TCut* STCut::GetTCut() const {
 }
 
 const std::vector<STCut>* STCut::GetMembers(bool deep) const { //if deep, it goes down the hierarchy of members, returning the base ones
-
-	if (!deep) return &members; //"normal" behavious
-	else {
-		std::vector<STCut>* basic_members = new std::vector<STCut>;
-		STCut::fill_basic_members(basic_members, &members);
-
-	return basic_members;
+	std::vector<STCut>* final_members = new std::vector<STCut>;
+	int i(0);
+	for (auto v : ptrs_to_members) {
+		if (v==NULL) {//it means the object does not exist anymore, so it has to be considered as its parts
+			std::cout<<"proviamo "<<members[i].GetTitle()<<std::endl;
+			if (members[i].GetMembers()->empty()){
+				final_members->emplace_back(members[i]);
+			} else {
+				for (auto &member : *members[i].GetMembers()){
+					final_members->emplace_back(member);
+				}
+			}	
+		} else { //it means the object still exists, so it has to be considered as a whole
+			final_members->emplace_back(*v);
+		}
+	i++;
 	}
+	return final_members;
  }
 
+/*
 void STCut::fill_basic_members (std::vector<STCut>* basic_members, const std::vector<STCut>* input_members) const {
 	for (auto member : *input_members) { //loop on members to find the deepest ones
 		const std::vector<STCut>* members_of_member = member.GetMembers();
@@ -45,7 +69,7 @@ void STCut::fill_basic_members (std::vector<STCut>* basic_members, const std::ve
 		else STCut::fill_basic_members(basic_members, members_of_member);
 	}
 }
-
+*/
 
 //"get" functions to emulate TCut's behaviour
 const char* STCut::GetTitle() const {
@@ -69,18 +93,112 @@ const char* STCut::GetName() const {
  	return *this;
  }
 
-  STCut& STCut::operator+=(const STCut& stcut) {
+  STCut& STCut::operator+=(STCut& rhs) {
+  	std::cout<<"operator+= called"<<std::endl;
+  	c+=(*rhs.GetTCut());
+  	/*
 	STCut this_copy = STCut(*this); //copy the current instance before altering it
  	members.clear();
  	members.emplace_back(this_copy); //add itself as a member, as it is now (to keep structure)
- 	c+=(*stcut.GetTCut()); //just call TCut's +=
- 	members.emplace_back(stcut);
+ 	ptrs_to_members.emplace_back(this);
+   	pointed_by.emplace_back(&ptrs_to_members.back());
+ 	members.emplace_back(rhs);
+  	ptrs_to_members.emplace_back(&rhs);
+    rhs.pointed_by.emplace_back(&ptrs_to_members.back());*/
  	return *this;
  }
 
 
 
+ //destructor
+ STCut::~STCut(){
+ 	std::cout<<"Destructor called on "<<this->GetTitle()<<"  "<<this<<std::endl;
+
+ 	//remove itself from pointed_by of other instances
+ 	for (auto &member : ptrs_to_members){
+ 		std::cout<<"---going in member "<<member<<std::endl;
+ 		if (member != NULL){
+ 			std::cout<<"...member: "<<member->GetTitle()<<"  "<<member<<std::endl;
+ 			auto v = member->pointed_by;
+ 			for (auto i: v) std::cout << i << ' ';
+ 			std::cout<<std::endl;
+ 			v.erase(std::find(v.begin(), v.end(), this));
+ 			for (auto i: v) std::cout << i << ' ';
+ 			std::cout<<std::endl;
+ 		}
+ 	}
+ 	//replace itself by NULL where it is pointed
+ 	for (STCut* pointing_stcut : this->pointed_by){
+ 		auto &v = pointing_stcut->ptrs_to_members;
+ 		for (auto &pointer : v) {
+ 			if (pointer == this){
+ 				std::cout<<"...replaced "<<pointer<<" with NULL in "<<pointing_stcut<<std::endl;
+ 				pointer = NULL;
+ 			}
+ 		}
+ 	}
+ }
+
+
+//friends
+
+  STCut operator+(const STCut lhs, const STCut rhs) {
+ 	std::cout<<"operator+ (friend) called"<<std::endl;
+ 	STCut result;
+ 	result.c=(*lhs.GetTCut()+*rhs.GetTCut());
+
+ 	result.members.emplace_back(lhs); //add itself as a member, as it is now (to keep structure)
+ 	result.ptrs_to_members.emplace_back(&lhs);
+ 	std::cout<<"+Spingo "<<&lhs<<" in "<<&result<<std::endl;
+   	lhs.pointed_by.emplace_back(&result);
+
+ 	result.members.emplace_back(rhs);
+  	result.ptrs_to_members.emplace_back(&rhs);
+  	std::cout<<"+Spingo "<<&rhs<<" in "<<&result<<std::endl;
+    rhs.pointed_by.emplace_back(&result);
+
+ 	return result;
+ }
+
+
+ /*
+ STCut operator+(STCut& lhs, STCut& rhs) {
+ 	std::cout<<"operator+ (friend) called"<<std::endl;
+ 	STCut result;
+ 	result.c=(*lhs.GetTCut()+*rhs.GetTCut());
+
+ 	result.members.emplace_back(lhs); //add itself as a member, as it is now (to keep structure)
+ 	result.ptrs_to_members.emplace_back(&lhs);
+ 	std::cout<<"+Spingo "<<&lhs<<" in "<<&result<<std::endl;
+   	lhs.pointed_by.emplace_back(&result);
+
+ 	result.members.emplace_back(rhs);
+  	result.ptrs_to_members.emplace_back(&rhs);
+  	std::cout<<"+Spingo "<<&rhs<<" in "<<&result<<std::endl;
+    rhs.pointed_by.emplace_back(&result);
+
+ 	return result;
+ }
+
+
+  STCut operator+(STCut&& lhs, STCut& rhs) {
+  	STCut lhs_lvalue = STCut(lhs);
+  	return operator+(lhs_lvalue,rhs);
+ }
+
+  STCut operator+(STCut& lhs, STCut&& rhs) {
+  	STCut rhs_lvalue = STCut(rhs);
+  	return operator+(lhs,rhs_lvalue);
+ }
+
+   STCut operator+(STCut&& lhs, STCut&& rhs) {
+  	STCut lhs_lvalue = STCut(lhs);
+   	STCut rhs_lvalue = STCut(rhs);
+  	return operator+(lhs_lvalue,rhs_lvalue);
+ }
+*/
 //friend functions
+ /*
   STCut operator+(const STCut& lhs, const char *rhs) {
   	return STCut(lhs) += rhs;
  }
@@ -92,3 +210,4 @@ const char* STCut::GetName() const {
   STCut operator+(const STCut& lhs, const STCut& rhs) {
   	return STCut(lhs) += rhs;
  }
+ */
--- tools/STCut.hpp
+++ tools/STCut.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "TCut.h"
+#include <iostream>
 
 class STCut {
 
@@ -17,22 +18,41 @@ class STCut {
 		const char* GetName() const;
 		const std::vector<STCut>* GetMembers(bool deep=false) const;
 
+		virtual ~STCut(); //destructor
+
 		STCut(const TCut&); //constructor from TCut
 		STCut(const char*); //constructor from char*
 		STCut(const STCut&); //copy constructor
 		STCut() = default; //default empty constructor
+		STCut& operator=(const STCut&) = default;
 
 		//+= operators
 		STCut& operator+=(const char*);
-		STCut& operator+=(const STCut &);
+		STCut& operator+=(STCut &);
 
+		friend STCut operator+(const STCut, const STCut); //I cannot pass the arguments by reference, because this would require (for this specific implementation)
+//to distinguish rvalues and lvalues, and this breaks when imported in python
+/*		friend STCut operator+(STCut&, STCut&);
+		friend STCut operator+(STCut&&, STCut&);
+		friend STCut operator+(STCut&, STCut&&);
+		friend STCut operator+(STCut&&, STCut&&);*/
+
+		/*
     	friend STCut operator+(const STCut&, const char*);
     	friend STCut operator+(const char*, const STCut&);
     	friend STCut operator+(const STCut&, const STCut&);
+		*/
+
+		//STCut* operator&() & = default;
+
+
+		std::vector<STCut*> pointed_by;
+		std::vector<STCut*> ptrs_to_members; //cannot use pointers to STCuts here. It's unsafe, if they point to a temporary object for example
 
 	private:
 		TCut c;
-		std::vector<STCut> members; //cannot use pointers to STCuts here. It's unsafe, if they point to a temporary object for example
+		std::vector<STCut> members; //FIX THIS LATER////cannot use pointers to STCuts here. It's unsafe, if they point to a temporary object for example
+
 		void fill_basic_members (std::vector<STCut>*, const std::vector<STCut>*) const;
 };
 
