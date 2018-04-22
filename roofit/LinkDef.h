0d57a9a4f6419703d06c44e4de921ff6eb4666fe
Simone Bifani <simone.bifani@cern.ch>
2018-04-22 17:16:01 +0100

    add RooBifurGauss, fix Poly, fix abs warnings

--- roofit/LinkDef.h
+++ roofit/LinkDef.h
@@ -12,8 +12,6 @@
 
 // ls *.h | awk '{ print "#pragma link C++ defined_in \""$1"\";" }'
 
-#pragma link C++ class RooAmorosoPdf+;
-#pragma link C++ defined_in "RooAmorosoPdf.h";
 #pragma link C++ class RooApollonios+;
 #pragma link C++ defined_in "RooApollonios.h";
 #pragma link C++ class RooExpAndGauss+;
@@ -26,8 +24,6 @@
 #pragma link C++ defined_in "RooIpatia.h";
 #pragma link C++ class RooJohnson+;
 #pragma link C++ defined_in "RooJohnson.h";
-//#pragma link C++ class RooLogGamma+;
-//#pragma link C++ defined_in "RooLogGamma.h";
 #pragma link C++ class RooMisIDGaussian+;
 #pragma link C++ defined_in "RooMisIDGaussian.h";
 #pragma link C++ class RooMomentumFractionPdf+;
--- roofit/roofit.hpp
+++ roofit/roofit.hpp
@@ -1,14 +1,12 @@
 #ifndef ROOFIT_HPP
 #define ROOFIT_HPP
 
-#include "RooAmorosoPdf.h"
 #include "RooApollonios.h"
 #include "RooExpAndGauss.h"
 #include "RooGeneralizedHyperbolic.h"
 #include "RooIpatia.h"
 #include "RooIpatia2.h"
 #include "RooJohnson.h"
-//#include "RooLogGamma.h"
 #include "RooMisIDGaussian.h"
 #include "RooDoubleCB.h"
 
