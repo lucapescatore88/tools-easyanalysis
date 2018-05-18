40d8451c533a38fe9bfe374697d0cc7722521c7c
Simone Bifani <simone.bifani@cern.ch>
2018-05-18 12:14:49 +0100

    fix isParInMap

--- tools/stringToPdf.cpp
+++ tools/stringToPdf.cpp
@@ -12,23 +12,16 @@ RooRealVar * addPar(string par, string parstr, Str2VarMap stval_list, Str2VarMap
     size_t pos = parstr.find(par + "[");
     if (par == "a2os") pos = parstr.find("a2[");
 
-cout << "addPar: " << par << " " << parstr << " " << option << endl;
-
     string vname = "";
     size_t posname = option.find("-n");
     if (posname != string::npos)
-        vname = option.substr(posname + 2, option.find("-", posname + 2) - (posname + 2) );
-
-cout << "addPar: vname = " << vname << endl;
+        vname = option.substr(posname + 2, option.find("-", posname + 2) - (posname + 2));
 
     string parMapName = "";
     if (myvars.size() > 0)
     {
-        if (par == "a2os") parMapName = isParInMap( "a2", myvars, vname );
-        else parMapName = isParInMap( par, myvars, vname );
-
-cout << "addPar: parMapName = " << parMapName << endl;
-
+        if (par == "a2os") parMapName = isParInMap("a2", myvars, vname);
+        else parMapName = isParInMap(par, myvars, vname);
         if (parMapName == "") cout << "addPar: *** WARNING *** " << par << " " << parstr << " " << option << endl;
     }
     if (parMapName != "") curpar = (RooRealVar *)myvars[parMapName];
@@ -197,7 +190,7 @@ Str2VarMap getPar(string typepdf_, TString namepdf_, RooRealVar * val, Str2VarMa
     if (endtype != string::npos) parstr = typepdf_.substr(endtype, string::npos);
     vector<string> pars = par_list[typepdf_.substr(0, endtype)];
 
-    for ( auto par : pars )
+    for (auto par : pars)
     {
         if (par == "a2os") parout["a2"] = addPar(par, parstr, stval_list, myvars, opt);
         else parout[par] = addPar(par, parstr, stval_list, myvars, opt);
